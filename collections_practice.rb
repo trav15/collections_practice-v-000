e9833ebd1065358fb4ac16b8b3589d9b06081d3d
Travis T <travistokuyama@gmail.com>
2018-09-15 17:49:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,4 +14,14 @@ def sort_array_desc(array)
   end
 end
 
-def 
+def sort_array_char_count
+  return new_array = array.sort do |a, b|
+    if a.length == b.length
+      0 
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length 
+      1 
+    end
+  end
+end
